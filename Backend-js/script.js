b3e670d770ec69cc2f32a95838c7e6dcb2f55c7f
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-04 21:55:31 +0530

    backend

--- Backend-js/script.js
+++ Backend-js/script.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.rmdir("./copy",{recursive: true}, function(err){
+fs.rm("./copy",{recursive: true}, function(err){
     if(err) console.error(err);
     else console.log("removed");
 })
