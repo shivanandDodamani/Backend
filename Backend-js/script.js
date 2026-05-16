9d0de78154d11d9266e4055d88bc7f78443941d4
shivanandDodamani <shivananddodamani635@gmail.com>
2026-05-16 19:56:54 +0530

    backend

--- Backend-js/script.js
+++ Backend-js/script.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("champion boy shiva");
+
+app.use(function(req, res,next){
+    console.log("middleware is running ");
+    next();
 })
 
-app.get("/propile", function(req, res){
-    res.send("is he");
+app.use(function(req, res,next){
+    console.log("middleware is running again one time ");
+    next();
+})
+
+
+app.get("/", function(req, res){
+    res.send("champions boy shivanand");
+})
+
+app.get("/about", function(req, res) {
+    res.send("about page chala");
 })
 
 app.listen(3000);
